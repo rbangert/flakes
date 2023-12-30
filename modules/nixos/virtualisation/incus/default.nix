5922491bb427b83e44e463ccb305bf1288b5e01f
rbangert <rbangert@proton.me>
2023-12-30 14:07:34 -0700

    dia: incus

--- modules/nixos/virtualisation/incus/default.nix
+++ modules/nixos/virtualisation/incus/default.nix
@@ -18,48 +18,48 @@ in {
       enable = true;
       preseed = {
         config = [
-        {
-          "core.https_address" = "10.0.0.97:8443";
-        }
+          {
+            "core.https_address" = "10.0.0.97:8443";
+          }
         ];
         networks = [
-        {
-          config = {
-            "ipv4.address" = "10.0.100.1/24";
-            "ipv4.nat" = "true";
-          };
-          name = "incusbr0";
-          type = "bridge";
-        }
+          {
+            config = {
+              "ipv4.address" = "10.0.100.1/24";
+              "ipv4.nat" = "true";
+            };
+            name = "incusbr0";
+            type = "bridge";
+          }
         ];
         profiles = [
-        {
-          devices = {
-            eth0 = {
-              name = "eth0";
-              network = "incusbr0";
-              type = "nic";
+          {
+            devices = {
+              eth0 = {
+                name = "eth0";
+                network = "incusbr0";
+                type = "nic";
+              };
+              root = {
+                path = "/";
+                pool = "default";
+                size = "35GiB";
+                type = "disk";
+              };
             };
-            root = {
-              path = "/";
-              pool = "default";
-              size = "35GiB";
-              type = "disk";
-            };
-          };
-          name = "default";
-        }
+            name = "default";
+          }
         ];
         storage_pools = [
-        {
-          config = {
-            source = "/var/lib/incus/storage-pools/default";
-          };
-          driver = "dir";
-          name = "default";
-        }
+          {
+            config = {
+              source = "/var/lib/incus/storage-pools/default";
+            };
+            driver = "dir";
+            name = "default";
+          }
         ];
-      }
+      };
     };
   };
 }
