f42f2e5ecf7da35f2603b108dd16863ace9b2768
rbangert <rbangert@proton.me>
2023-12-30 13:16:19 -0700

    dia: incus

--- modules/nixos/virtualisation/incus/default.nix
+++ modules/nixos/virtualisation/incus/default.nix
@@ -16,6 +16,49 @@ in {
   config = mkIf cfg.enable {
     virtualisation.incus = {
       enable = true;
+      preseed = {
+        config = [
+          "core.https_address" = "0.0.0.0:8443";
+        ];
+        networks = [
+        {
+          config = {
+            "ipv4.address" = "10.0.100.1/24";
+            "ipv4.nat" = "true";
+          };
+          name = "incusbr0";
+          type = "bridge";
+        }
+        ];
+        profiles = [
+        {
+          devices = {
+            eth0 = {
+              name = "eth0";
+              network = "incusbr0";
+              type = "nic";
+            };
+            root = {
+              path = "/";
+              pool = "default";
+              size = "35GiB";
+              type = "disk";
+            };
+          };
+          name = "default";
+        }
+        ];
+        storage_pools = [
+        {
+          config = {
+            source = "/var/lib/incus/storage-pools/default";
+          };
+          driver = "dir";
+          name = "default";
+        }
+        ];
+      }
+        }
     };
   };
 }
