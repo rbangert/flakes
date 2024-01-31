4222a0a9b5b8ab1130895c8927d90ee738f9c9d6
rbangert <rbangert@proton.me>
2024-01-31 04:42:24 -0700

    fix: herse secrets

--- modules/nixos/containers/mattermost/default.nix
+++ modules/nixos/containers/mattermost/default.nix
@@ -14,36 +14,53 @@ in {
   };
 
   config = mkIf cfg.enable {
-    virtualisation.oci-containers = {
-      containers = {
-        "mattermost" = {
-          image = "mattermost/mattermost-team-edition:9.4";
-          ports = [
-            "8065:8065"
-            "8443:8443/udp"
-            "8443:8443/tcp"
-          ];
-          volumes = [
-            "mattermost-data:/mattermost"
-          ];
-          environmentFiles = [config.sops.secrets.mattermostEnv.path];
-        };
-        "mm-postgres" = {
-          image = "postgres/postgres:16.1-alpine";
-          volumes = [
-            "mattermost-pgsql-data:/var/lib/postgresql/data"
-          ];
-          environmentFiles = [config.sops.secrets.mattermostEnv.path];
+    containers.mattermost = {
+      autoStart = true;
+      localAddress = "127.0.0.1";
+      config = {
+        config,
+        pkgs,
+        ...
+      }: {
+        services.mattermost = {
+          enable = true;
+          siteName = "DMA/RussCorp";
+          siteUrl = "https://mm.rr-sv.win";
+          matterircd.enable = true;
         };
       };
     };
+
+    # virtualisation.oci-containers = {
+    #   containers = {
+    #     "mattermost" = {
+    #       image = "mattermost/mattermost-team-edition:9.4";
+    #       ports = [
+    #         "8065:8065"
+    #         "8443:8443/udp"
+    #         "8443:8443/tcp"
+    #       ];
+    #       volumes = [
+    #         "mattermost-data:/mattermost"
+    #       ];
+    #       environmentFiles = [config.sops.secrets.mattermostEnv.path];
+    #     };
+    #     "mm-postgres" = {
+    #       image = "postgres/postgres:16.1-alpine";
+    #       volumes = [
+    #         "mattermost-pgsql-data:/var/lib/postgresql/data"
+    #       ];
+    #       environmentFiles = [config.sops.secrets.mattermostEnv.path];
+    #     };
+    #   };
+    # };
 
     services.nginx.virtualHosts = {
       "mm.rr-sv.win" = {
         forceSSL = true;
         enableACME = true;
         locations."/" = {
-          proxyPass = "http://127.0.0.1:8030";
+          proxyPass = "http://127.0.0.1:8065";
         };
       };
     };
