aa6255e4ae3340d8582b604c2edc8265a343e450
rbangert <rbangert@proton.me>
2024-02-05 01:13:10 -0700

    feat: mattermost-plugin-jitsi

--- modules/nixos/containers/mattermost/default.nix
+++ modules/nixos/containers/mattermost/default.nix
@@ -34,7 +34,7 @@ in {
               version = "2.0.1";
               src = pkgs.fetchzip {
                 url = "https://github.com/mattermost/mattermost-plugin-jitsi/releases/download/v2.0.1/jitsi-2.0.1.tar.gz";
-                sha256 = "sha256-iVmW9ZdNnzq08c929Tf1+xnA7cHWuujbn8BCpf6Zzeo=";
+                sha256 = "";
               };
 
               installPhase = ''
