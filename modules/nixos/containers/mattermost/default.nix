92c7a41d54652de17ee5e6f9d5d0b6f57f003526
rbangert <rbangert@proton.me>
2024-02-05 01:05:12 -0700

    feat: mattermost-plugin-jitsi

--- modules/nixos/containers/mattermost/default.nix
+++ modules/nixos/containers/mattermost/default.nix
@@ -29,12 +29,18 @@ in {
           matterircd.enable = true;
           mutableConfig = true;
           plugins = [
-            (pkgs.stdenvNoCC.mkDerivation {
+            (pkgs.stdenv.mkDerivation {
               name = "mattermost-plugin-jitsi";
-              src = fetchzip {
+              version = "2.0.1";
+              src = pkgs.fetchzip {
                 url = "https://github.com/mattermost/mattermost-plugin-jitsi/releases/download/v2.0.1/jitsi-2.0.1.tar.gz";
                 sha256 = "sha256-hLCSs3HsCSlb+ILzx37MXBNyGZCVrL9a080NZLfILUk=";
               };
+
+              installPhase = ''
+                mkdir -p $out
+                cp -a * $out
+              '';
             })
           ];
         };
