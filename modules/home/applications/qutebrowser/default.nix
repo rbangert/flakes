b79f7cab6c20a59cc08fa2504064ea89610e8d7e
Russ Bangert <rbangert@proton.me>
2024-07-11 20:08:50 -0700

    dia push

---
+++ modules/home/applications/qutebrowser/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  config,
+  pkgs,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.applications.qutebrowser;
+in {
+  options.${namespace}.applications.qutebrowser = {
+    enable = mkEnableOption "qutebrowser";
+  };
+  config = mkIf cfg.enable {
+    # programs.qutebrowser = {
+    #   enable = true;
+    #   # Disable to prevent loading default key bindings.
+    #   enableDefaultBindings = true;
+    # };
+
+    # home.sessionVariables = {DEFAULT_BROWSER = "${pkgs.qutebrowser}/bin/qutebrowser";};
+
+    environment.systemPackages = with pkgs; [
+      qutebrowser
+    ];
+
+    # home.file.".config/qutebrowser" = {
+    #   source = ../../../../config/qutebrowser;
+    #   recursive = true;
+    # };
+  };
+}
--- systems/x86_64-linux/io/default.nix
+++ systems/x86_64-linux/io/default.nix
@@ -106,9 +106,6 @@ with lib.${namespace}; {
   };
 
   environment.systemPackages = with pkgs; [
-    python312Full
-    python312Packages.pip
-    python312Packages.setuptools
     qutebrowser
     neovim
     firefox-bin
