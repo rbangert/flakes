c235e0bce6b2e284a977ad9b4b65419e30fc39c4
Russ Bangert <rbangert@proton.me>
2024-07-16 23:43:18 -0700

    vaultwarden

--- modules/nixos/containers/vaultwarden/default.nix
+++ modules/nixos/containers/vaultwarden/default.nix
@@ -12,11 +12,32 @@ in {
       containers = {
         "vaultwarden" = {
           image = "vaultwarden/server:1.31.0-alpine";
-          ports = [ "8119:80" ];
+          ports = [ "8119:80" "3012:3012" ];
           volumes = [ "vaultwarden-data:/data" ];
           environment = {
             SIGNUPS_ALLOWED = "false";
             IP_HEADER = "none";
+            DOMAIN = "https://vault.russellb.dev";
+            WEBSOCKET_ENABLED = true;
+            WEBSOCKET_ADDRESS = "0.0.0.0";
+            WEBSOCKET_PORT = 3012;
+            # ADMIN_TOKEN = (import /etc/nixos/secret/bitwarden.nix).ADMIN_TOKEN;
+            # YUBICO_CLIENT_ID =
+            #   (import /etc/nixos/secret/bitwarden.nix).YUBICO_CLIENT_ID;
+            # YUBICO_SECRET_KEY =
+            #   (import /etc/nixos/secret/bitwarden.nix).YUBICO_SECRET_KEY;
+            # YUBICO_SERVER = "https://api.yubico.com/wsapi/2.0/verify";
+            # SMTP_HOST = "mx.example.com";
+            # SMTP_FROM = "bitwarden@example.com";
+            # SMTP_FROM_NAME = "Bitwarden_RS";
+            # SMTP_PORT = 587;
+            # SMTP_SECURITY = starttls;
+            # SMTP_USERNAME =
+            #   (import /etc/nixos/secret/bitwarden.nix).SMTP_USERNAME;
+            # SMTP_PASSWORD =
+            #   (import /etc/nixos/secret/bitwarden.nix).SMTP_PASSWORD;
+            # SMTP_TIMEOUT = 15;
+            # ROCKET_PORT = 8812;
           };
         };
       };
