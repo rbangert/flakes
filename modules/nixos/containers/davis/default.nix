94183f210fe6c49dbccb45373cff0cf875278b15
Russ Bangert <rbangert@proton.me>
2024-07-20 22:53:55 -0700

    misc and davis

--- modules/nixos/containers/davis/default.nix
+++ modules/nixos/containers/davis/default.nix
@@ -24,6 +24,20 @@ in {
           appSecretFile = "/run/secrets/davis-secret";
           nginx = { };
         };
+
+        system.stateVersion = "24.05";
+
+        networking = {
+          firewall = {
+            enable = true;
+            allowedTCPPorts = [ 8000 9000 ];
+          };
+          # Use systemd-resolved inside the container
+          # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+          useHostResolvConf = lib.mkForce false;
+        };
+
+        services.resolved.enable = true;
       };
     };
 
