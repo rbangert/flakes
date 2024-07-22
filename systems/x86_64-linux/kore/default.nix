bb3882d07ce33d2e6b8e4699e19d7acdcbe6276b
Russ Bangert <rbangert@proton.me>
2024-07-22 02:05:17 -0700

    kore

--- systems/x86_64-linux/kore/default.nix
+++ systems/x86_64-linux/kore/default.nix
@@ -49,6 +49,8 @@ with lib.rr-sv; {
   zramSwap.enable = true;
   networking.hostName = "kore";
   networking.domain = "kore.russellb.dev";
-  users.users.root.openssh.authorizedKeys.keys = [ (fetchKeys "rbangert") ];
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOdfj6SbSBSWs2medcA8jKdFmVT1CL8l6iXTCyPUsw7y russ@rr-sv.win"
+  ];
   system.stateVersion = "24.05";
 }
