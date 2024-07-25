fb962de705cd61c1c3ce97f2e6dca51ece6ef3f8
Russ Bangert <rbangert@proton.me>
2024-07-25 16:34:46 -0700

    dia: itflow

--- modules/nixos/services/grafana/default.nix
+++ modules/nixos/services/grafana/default.nix
@@ -23,8 +23,9 @@ in {
         forceSSL = true;
         enableACME = true;
         locations."/" = {
-          proxyPass =
-            "http://127.0.0.1:${config.services.grafana.settings.server.http_port}";
+          proxyPass = "http://127.0.0.1:${
+              toString config.services.grafana.settings.server.http_port
+            }";
           proxyWebsockets = true;
         };
       };
