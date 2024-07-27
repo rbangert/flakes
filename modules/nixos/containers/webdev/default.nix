a676051595fcd8d5f7b6941f3f0c2d800be2f2b4
Russ Bangert <rbangert@proton.me>
2024-07-27 01:28:38 -0700

    dia: webdev

--- modules/nixos/containers/webdev/default.nix
+++ modules/nixos/containers/webdev/default.nix
@@ -4,6 +4,49 @@ with lib.rr-sv;
 let
   cfg = config.rr-sv.containers.webdev;
   domain = "wp-dev.rr-sv.win";
+
+  # Auxiliary functions
+  fetchPackage = { name, version, hash, isTheme }:
+    pkgs.stdenv.mkDerivation rec {
+      inherit name version hash;
+      src = let type = if isTheme then "theme" else "plugin";
+      in pkgs.fetchzip {
+        inherit name version hash;
+        url = "https://downloads.wordpress.org/${type}/${name}.${version}.zip";
+      };
+      installPhase = "mkdir -p $out; cp -R * $out/";
+    };
+
+  fetchPlugin = { name, version, hash }:
+    (fetchPackage {
+      name = name;
+      version = version;
+      hash = hash;
+      isTheme = false;
+    });
+
+  fetchTheme = { name, version, hash }:
+    (fetchPackage {
+      name = name;
+      version = version;
+      hash = hash;
+      isTheme = true;
+    });
+
+  # Plugins
+  google-site-kit = (fetchPlugin {
+    name = "google-site-kit";
+    version = "1.103.0";
+    hash = "sha256-8QZ4XTCKVdIVtbTV7Ka4HVMiUGkBYkxsw8ctWDV8gxs=";
+  });
+
+  # Themes
+  astra = (fetchTheme {
+    name = "astra";
+    version = "4.1.5";
+    hash = "sha256-X3Jv2kn0FCCOPgrID0ZU8CuSjm/Ia/d+om/ShP5IBgA=";
+  });
+
 in {
   options.rr-sv.containers.webdev = with types; {
     enable = mkBoolOpt false "Whether or not to enable webdev";
