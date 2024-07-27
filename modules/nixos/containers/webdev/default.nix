f660cde6803f1d7437f5c60df57595db823d5d5d
Russ Bangert <rbangert@proton.me>
2024-07-27 01:26:19 -0700

    dia: webdev

--- modules/nixos/containers/webdev/default.nix
+++ modules/nixos/containers/webdev/default.nix
@@ -3,57 +3,25 @@ with lib;
 with lib.rr-sv;
 let
   cfg = config.rr-sv.containers.webdev;
-  app = "dev";
-  appDomain = "dev.russellb.dev";
-  dataDir = "/var/www/${app}";
-
+  domain = "wp-dev.rr-sv.win";
 in {
   options.rr-sv.containers.webdev = with types; {
     enable = mkBoolOpt false "Whether or not to enable webdev";
   };
 
   config = mkIf cfg.enable {
-    services.phpfpm.pools.${app} = {
-      user = app;
-      settings = {
-        "listen.owner" = config.services.nginx.user;
-        "listen.group" = config.services.nginx.group;
-        "listen.mode" = "0660";
-        "catch_workers_output" = 1;
+    services = {
+      nginx.virtualHosts.${domain} = {
+        enableACME = true;
+        forceSSL = true;
       };
-    };
 
-    users.groups.${app}.members = [ "${app}" ];
-    users.users.${app} = {
-      isSystemUser = true;
-      group = "${app}";
-    };
-    users.users.nginx.extraGroups = [ "${app}" ];
-
-    services.nginx = {
-      enable = true;
-
-      virtualHosts = {
-        ${appDomain} = {
-          root = "${dataDir}";
-
-          extraConfig = ''
-            index index.php;
-          '';
-
-          locations."~ ^(.+\\.php)(.*)$" = {
-            extraConfig = ''
-              # Check that the PHP script exists before passing it
-              try_files $fastcgi_script_name =404;
-              include ${config.services.nginx.package}/conf/fastcgi_params;
-              fastcgi_split_path_info  ^(.+\.php)(.*)$;
-              fastcgi_pass unix:${config.services.phpfpm.pools.${app}.socket};
-              fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
-              fastcgi_param  PATH_INFO        $fastcgi_path_info;
-
-              include ${pkgs.nginx}/conf/fastcgi.conf;
-            '';
-          };
+      wordpress = {
+        webserver = "nginx";
+        sites."${domain}" = {
+          plugins = { inherit google-site-kit; };
+          themes = { inherit astra; };
+          settings = { WP_DEFAULT_THEME = "astra"; };
         };
       };
     };
